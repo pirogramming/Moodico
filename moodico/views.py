c93d7b3a9bac791ba7a56100089aec9fbc17a0a4
namu627 <0627eric@naver.com>
2025-07-29 23:21:39 +0900

    [fix] 일부 페이지  안보이는 문제 해결(views.py 수정)

--- moodico/views.py
+++ moodico/views.py
@@ -3,27 +3,38 @@ from django.http import HttpResponse
 import json
 import os
 
+def color_matrix_explore(request):
+    return render(request, 'color_matrix.html')
+
+# Create your views here.
+
 
 def main(request):
+    """메인 페이지 뷰"""
     return render(request, 'main.html')
 
 def login_view(request):
+    """로그인 페이지 뷰"""
     return render(request, 'login.html')
 
 def signup_view(request):
+    """회원가입 페이지 뷰"""
     return render(request, 'signup.html')
 
 def my_item_recommendation(request):
+    """내 아이템 기반 추천 페이지 뷰"""
     # 백엔드 연동을 위해 임시로 빈 리스트 전달
     recommended_items = []
     search_results = []
     return render(request, 'upload.html', {'search_results': search_results, 'recommended_items': recommended_items})
 
 def mood_test(request):
+    """무드 테스트 페이지 뷰"""
     return render(request, 'mood_test.html')
 
 def mood_result(request):
     # 무드 테스트 결과 데이터 (백엔드에서 받아올 예정)
+    """무드 테스트 결과 페이지 뷰"""
     mood_data = {
         'mood_name': '로맨틱',
         'mood_description': '부드럽고 여성스러운 사랑스러운 느낌',
@@ -37,10 +48,13 @@ def mood_result(request):
     }
     return render(request, 'mood_result.html', mood_data)
 
+
 def color_matrix_explore(request):
+    """색상 매트릭스 페이지 뷰"""
     return render(request, 'color_matrix.html')
 
 def product_detail(request, product_id):
+    """제품 상세 페이지 뷰"""
     # 제품 상세 정보 (백엔드에서 받아올 예정)
     product = {
         'id': product_id,
@@ -51,39 +65,6 @@ def product_detail(request, product_id):
     }
     return render(request, 'detail.html', {'product': product})
 
-# Create your views here.
-
-def main(request):
-    """메인 페이지 뷰"""
-    return render(request, 'base.html')
-
-def login_view(request):
-    """로그인 페이지 뷰"""
-    return render(request, 'login.html')
-
-def signup_view(request):
-    """회원가입 페이지 뷰"""
-    return render(request, 'signup.html')
-
-def my_item_recommendation(request):
-    """내 아이템 기반 추천 페이지 뷰"""
-    return render(request, 'base.html')
-
-def mood_test(request):
-    """무드 테스트 페이지 뷰"""
-    return render(request, 'mood_test.html')
-
-def mood_result(request):
-    """무드 테스트 결과 페이지 뷰"""
-    return render(request, 'mood_result.html')
-
-def color_matrix_explore(request):
-    """색상 매트릭스 페이지 뷰"""
-    return render(request, 'color_matrix.html')
-
-def product_detail(request, product_id):
-    """제품 상세 페이지 뷰"""
-    return render(request, 'detail.html', {'product_id': product_id})
 
 def product_list(request):
     json_path = os.path.join('static', 'data', 'products.json')
@@ -92,3 +73,4 @@ def product_list(request):
         products = json.load(f)
     
     return render(request, 'product_list.html', {'products': products})
+
