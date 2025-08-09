dd7285d0fa3f9f026d8450a259c06e2ae431e752
ZinYan <zinwaiyan2226@gmail.com>
2025-08-09 16:57:55 +0900

    [deploy]테스트용 secret-key 추가

--- moodico_project/settings.py
+++ moodico_project/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+if os.getenv('GITHUB_ACTIONS') == 'true':  # running in GitHub Actions
+    SECRET_KEY = config('DJANGO_SECRET_KEY', default='insecure-ci-only-secret')
+else:  # production or local dev
+    SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
